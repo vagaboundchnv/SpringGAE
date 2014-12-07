df26461fd4947a6cc4c67437d7faba76ff0f28a6
vagaboundchnv <chalasaninaveenz@gmail.com>
2014-12-07 14:47:11 +0530

    Add Files

---
+++ src/com/MyMainController.java
@@ -0,0 +1,35 @@
+package com;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/movie")
+public class MyMainController {
+	//DI via Spring
+
+	String message;
+	 
+		@RequestMapping(value="/{name}", method = RequestMethod.GET)
+		public String getMovie(@PathVariable String name, ModelMap model) {
+	 
+			model.addAttribute("movie", name);
+			model.addAttribute("message", this.message);
+			System.out.println("Howdey");
+			//return to jsp page, configured in mvc-dispatcher-servlet.xml, view resolver
+			return "list";
+			
+		}
+		 @RequestMapping
+		    public  String mydefault() {
+		        return "Hi Book!";
+		    }
+	 
+		public void setMessage(String message) {
+			this.message = message;
+		}
+}
